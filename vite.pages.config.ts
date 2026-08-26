a8ce04d0e17b92066731eb656577f5f0b7c5f7f7
alijvy <aliw.oray@gmail.com>
2026-08-26 16:15:19 +0330

    Add GitHub Pages build configuration

---
+++ vite.pages.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig({
+  base: '/Alireza-Oraee/',
+  root: fileURLToPath(new URL('./github-pages', import.meta.url)),
+  publicDir: fileURLToPath(new URL('./public', import.meta.url)),
+  plugins: [react()],
+  build: {
+    outDir: fileURLToPath(new URL('./dist-pages', import.meta.url)),
+    emptyOutDir: true,
+  },
+});
